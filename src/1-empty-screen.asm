5afb692440357bd14c1659f33c60dc74ddb02ee2
Scotty Rogers <scottyrogers.me@gmail.com>
2023-11-13 21:50:45 -0700

    Update to formatting spaces

--- src/1-empty-screen.asm
+++ src/1-empty-screen.asm
@@ -2,12 +2,12 @@ BasicUpstart2(init)
 
 		*= $c000 "Init"
 
-		.const CHAR_SPACE 			= $20
-		.const SCREEN_TOP_LEFT_ADDR = $0400
+		.const CHAR_SPACE 				= $20
+		.const SCREEN_TOP_LEFT_ADDR 	= $0400
 
 init:
 		lda #CHAR_SPACE
-		ldx #$fa					// 250 byte chunks
+		ldx #$fa							// 250 byte chunks
 !:		sta SCREEN_TOP_LEFT_ADDR-1, x
 		sta SCREEN_TOP_LEFT_ADDR+249, x
 		sta SCREEN_TOP_LEFT_ADDR+499, x
